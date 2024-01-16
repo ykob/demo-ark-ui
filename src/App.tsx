a1838c570aaa23ab855b25d2c8eb67bfdb2ae127
ykob <info@tplh.net>
2024-01-16 23:02:29 +0900

    update date picker to add ref and portal container prop.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
 import { DatePicker } from '@/components/ark-ui';
 import { UISection, UISectionHeading } from '@/components/common';
 import { PageHeader } from '@/components/layouts';
+import { useRef } from 'react';
 import { CalendarDate } from '@internationalized/date';
 import { css, cx } from 'styled-system/css';
 import { container, flex } from 'styled-system/patterns';
 
 export default function App() {
+  const ref = useRef<HTMLDivElement>(null);
+
   return (
     <div className={cx(container(), styles.container)}>
       <PageHeader />
       <UISection>
         <UISectionHeading>DatePicker</UISectionHeading>
-        <div className={cx(css({ flex: 1, gap: 4 }), flex())}>
-          <DatePicker locale="ja-JP" />
-          <DatePicker locale="ja-JP" defaultValue={['2024-01-10']} />
+        <div className={cx(css({ flex: 1, gap: 4 }), flex())} ref={ref}>
+          <DatePicker portalContainer={ref} locale="ja-JP" />
           <DatePicker
+            portalContainer={ref}
+            locale="ja-JP"
+            defaultValue={['2024-01-10']}
+          />
+          <DatePicker
+            portalContainer={ref}
             locale="ja-JP"
             min={new CalendarDate(2020, 1, 1)}
             max={new CalendarDate(2030, 1, 1)}
           />
-          <DatePicker />
+          <DatePicker portalContainer={ref} />
         </div>
       </UISection>
     </div>
--- src/components/ark-ui/date-picker/index.tsx
+++ src/components/ark-ui/date-picker/index.tsx
@@ -1,4 +1,5 @@
 import { FloatingBox } from '@/components/common';
+import { RefObject } from 'react';
 import { DatePicker, type DatePickerProps, Portal } from '@ark-ui/react';
 import { css, cx } from 'styled-system/css';
 import { DatePickerControl } from './control';
@@ -6,13 +7,20 @@ import { DatePickerDayView } from './day-view';
 import { DatePickerMonthView } from './month-view';
 import { DatePickerYearView } from './year-view';
 
-type ArkDatePickerProps = DatePickerProps & React.RefAttributes<HTMLDivElement>;
+type ArkDatePickerProps = DatePickerProps &
+  React.RefAttributes<HTMLDivElement> & {
+    portalContainer?: RefObject<HTMLElement>;
+  };
 
-export const ArkDatePicker = ({ className, ...props }: ArkDatePickerProps) => {
+export const ArkDatePicker = ({
+  className,
+  portalContainer,
+  ...props
+}: ArkDatePickerProps) => {
   return (
     <DatePicker.Root className={cx(styles.wrap, className)} {...props}>
       <DatePickerControl />
-      <Portal>
+      <Portal container={portalContainer}>
         <DatePicker.Positioner>
           <DatePicker.Content>
             <FloatingBox>
