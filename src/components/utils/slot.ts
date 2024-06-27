90017a48687516cf6b5c5c3564b04bc4a2bbb09c
Yoichi Kobayashi <yoichi_kobayashi@yayoi-kk.co.jp>
2024-06-27 13:33:05 +0900

    added slot util component.

---
+++ src/components/utils/slot.ts
@@ -0,0 +1,34 @@
+import {
+  Children,
+  cloneElement,
+  isValidElement,
+  type HTMLAttributes,
+  type ReactNode,
+} from 'react';
+import { cx } from 'styled-system/css';
+
+export type AsChildProps<T> =
+  | ({ asChild?: false | undefined } & T)
+  | { asChild: true; children: ReactNode };
+
+type SlotProps = HTMLAttributes<HTMLElement> & {
+  children?: ReactNode;
+};
+
+export function Slot({ children, className, style, ...props }: SlotProps) {
+  if (isValidElement(children)) {
+    return cloneElement(children, {
+      ...props,
+      ...children.props,
+      style: {
+        ...style,
+        ...children.props.style,
+      },
+      className: cx(className, children.props.className),
+    });
+  }
+  if (Children.count(children) > 1) {
+    Children.only(null);
+  }
+  return null;
+}
